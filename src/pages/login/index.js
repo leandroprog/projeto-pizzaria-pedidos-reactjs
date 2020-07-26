d17825b767b5e7f87d98dc950862a6d38cb5dc91
Leandro Rocha <prog.leandro@gmail.com>
2020-07-26 00:23:55 -0300

    Criando estilo para a tela de login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
-import { Button } from '@material-ui/core'
+import { Button, Grid } from '@material-ui/core'
+
+import { Container } from './styles'
 
 import { ReactComponent as Logo } from 'assets/logo-react-zzaria.svg'
 
 const Login = () => (
-  <>
-    <Logo />
-    <Button>Entrar com GitHub</Button>
-  </>
+  <Container>
+    <Grid container direction='column' alignItems='center' spacing={5}>
+      <Grid item>
+        <Logo />
+      </Grid>
+      <Grid item>
+        <Button>Entrar com GitHub</Button>
+      </Grid>
+    </Grid>
+  </Container>
 )
 
 export default Login
