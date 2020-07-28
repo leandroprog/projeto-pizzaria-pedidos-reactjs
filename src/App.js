d87d5ef1a8f6678376b5217c630f231d0154ec8a
Leandro Rocha <prog.leandro@gmail.com>
2020-07-27 23:16:09 -0300

    Evitar redirects desnecessários

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React, { Suspense, lazy, useEffect, useContext } from 'react'
-import { Route, Switch } from 'react-router-dom'
+import React, { Suspense, lazy, useEffect, useContext, useState } from 'react'
+import t from 'prop-types'
+import { Route, Switch, Redirect } from 'react-router-dom'
 import { LinearProgress } from '@material-ui/core'
 import firebase from 'services/firebase'
 
@@ -8,21 +9,41 @@ import { AuthContext } from 'contexts/auth'
 const MainPage = lazy(() => import('pages/main'))
 const Login = lazy(() => import('pages/login'))
 
-function App () {
-  const { setUser } = useContext(AuthContext)
+function App ({ location }) {
+  const { user, setUser, logout } = useContext(AuthContext)
+  const [didCheckUserIn, setDidCheckUserIn] = useState(false)
 
   useEffect(() => {
-    firebase.auth().onIdTokenChanged((user) => {
+    firebase.auth().onAuthStateChanged((user) => {
       console.log('usuário logado', user)
-      if (user) {
-        setUser({
-          displayName: user.displayName
-        })
-      } else {
-        setUser(null)
-      }
+      setUser(user)
+      setDidCheckUserIn(true)
     })
-  }, [setUser])
+
+    window.logout = logout
+  }, [logout, setUser])
+
+  if (!didCheckUserIn) {
+    return <h1>Carregando</h1>
+  }
+
+  if (user) {
+    console.log('usuário logado')
+    // eslint-disable-next-line react/prop-types
+    if (location.pathname === '/login') {
+      console.log('usuário logado e está na página de login')
+      return <Redirect to='/' />
+    } else {
+      console.log('Está logado mas não está na página de login')
+    }
+  } else {
+    console.log('Deslogado')
+    if (location.pathname !== '/login') {
+      return <Redirect to='/login' />
+    } else {
+      console.log('Não está logado e está na página de login')
+    }
+  }
 
   return (
     <>
@@ -35,6 +56,10 @@ function App () {
 
     </>
   )
+}
+
+App.prototypes = {
+  location: t.object.isRequired
 }
 
 export default App
