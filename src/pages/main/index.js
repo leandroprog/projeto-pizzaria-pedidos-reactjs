86c01b9c841916052edee53c0a81a924fcec47f2
Leandro Rocha <prog.leandro@gmail.com>
2020-07-28 23:38:15 -0300

    Estilizando os cards

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -1,14 +1,27 @@
 import React, { useState, useCallback, useContext } from 'react'
-import { AppBar, IconButton, Typography, Menu, MenuItem } from '@material-ui/core'
+import {
+  AppBar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Grid,
+  withStyles
+
+} from '@material-ui/core'
 
 import { AuthContext } from 'contexts/auth'
 
-import { LogoContainer, Logo, Toolbar } from './styles'
+import { Conteiner, LogoContainer, Logo, Toolbar, Divider, PaperPizza } from './styles'
+
+const Spacer = withStyles((theme) => ({
+  main: theme.mixins.toolbar
+}))(({ classes }) => <div className={classes.main} />)
 
 const Main = () => {
   const [anchorElement, setAnchorElement] = useState(null)
   const { logout, user } = useContext(AuthContext)
-
+  const userName = user.displayName.split(' ')[0]
   const handleOpenMenu = useCallback((e) => {
     setAnchorElement(e.target)
   }, [])
@@ -18,26 +31,72 @@ const Main = () => {
   }, [])
 
   return (
-    <AppBar>
-      <Toolbar>
-        <LogoContainer>
-          <Logo />
-        </LogoContainer>
-        <Typography color='inherit'>Olá {user.displayName.split(' ')[0]} =)</Typography>
-        <IconButton color='inherit' onClick={handleOpenMenu}>
-          <span className='material-icons'>
-            account_circle
-          </span>
-        </IconButton>
-        <Menu
-          open={!!anchorElement} onClose={handleClose}
-          anchorEl={anchorElement}
-        >
-          <MenuItem onClick={logout}>Sair</MenuItem>
-        </Menu>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar>
+        <Toolbar>
+          <LogoContainer>
+            <Logo />
+          </LogoContainer>
+          <Typography color='inherit'>Olá {userName} =)</Typography>
+          <IconButton color='inherit' onClick={handleOpenMenu}>
+            <span className='material-icons'>account_circle</span>
+          </IconButton>
+          <Menu
+            open={!!anchorElement}
+            onClose={handleClose}
+            anchorEl={anchorElement}
+          >
+            <MenuItem onClick={logout}>Sair</MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
+
+      <Spacer />
+
+      <Conteiner>
+        <Grid container direction='column' alignItems='center'>
+
+          <Typography variant='h3' gutterBottom>O vai ser hoje, {userName} =)</Typography>
+          <Typography variant='h4' gutterBottom>Escolha o tamanho da pizza</Typography>
+        </Grid>
+
+        <Grid container spacing={2}>
+          {pizzaSizes.map((pizza) => (
+            <Grid item key={pizza.id} xs={4}>
+              <PaperPizza>
+                <div>{pizza.size}</div>
+                <Divider />
+                <Typography variant='h5'>{pizza.name}</Typography>
+                <Typography>{pizza.slices} fatias, {pizza.flavours} sabores</Typography>
+              </PaperPizza>
+            </Grid>
+          ))}
+        </Grid>
+      </Conteiner>
+    </>
   )
 }
+
+const pizzaSizes = [{
+  id: 1,
+  name: 'pequena',
+  size: 28,
+  slices: 2,
+  flavours: 1
+},
+{
+  id: 2,
+  name: 'Média',
+  size: 30,
+  slices: 6,
+  flavours: 2
+},
+{
+  id: 3,
+  name: 'Grande',
+  size: 32,
+  slices: 8,
+  flavours: 3
+}]
 
 export default Main
--- src/pages/main/styles.js
+++ src/pages/main/styles.js
@@ -1,7 +1,11 @@
 import styled from 'styled-components'
-import { Toolbar as MaterialToolbar } from '@material-ui/core'
+import { Toolbar as MaterialToolbar, Divider as MaterialDivider, Paper } from '@material-ui/core'
 
 import { ReactComponent as logo } from 'assets/logo-react-zzaria.svg'
+
+export const Conteiner = styled.div`
+  padding: 80px 20px 20px;
+`
 
 export const LogoContainer = styled.div`
 flex-grow: 1;
@@ -22,4 +26,16 @@ export const Toolbar = styled(MaterialToolbar)`
   margin: 0 auto;
   max-width: 960px;
   width: 100%;
+`
+
+export const Divider = styled(MaterialDivider)`
+  margin: 20px 0;
+  width: 100%;
+`
+
+export const PaperPizza = styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 20px 0;
 `
